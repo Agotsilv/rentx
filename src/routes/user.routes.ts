3a9f8f0a4c888bbb6feaeac1e820fee566edd420
Tiago Oliveira <94474189+Agotsilv@users.noreply.github.com>
2023-02-02 13:33:41 -0300

    feat: created avatar for users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import UploadConfig from '../config/upload';
+import { UpdateUserAvatarController } from '../modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController';
 import { CreateUserController } from '../modules/accounts/UseCases/CreateUser/CreateUserController';
+import { ensureAuthenticated } from '../middlerwares/ensureAuthenticated';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+const uploadAvatar = multer(UploadConfig.upload('./tmp/avatar'));
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle,
+);
 
 export { usersRoutes };
